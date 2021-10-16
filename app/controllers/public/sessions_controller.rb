2ef04e487b2ec540b9b4b96d4166cfdb484d94fe
“takato15” <gshawks21@gmail.com>
2021-10-16 05:46:23 +0000

    Revert "Merge pull request #10 from takenoko-sato-naganocake/feature-devise"
    This reverts commit 84306f9d915ace45e4a4e2763f5a60791e794398, reversing
    changes made to b5bfdcb7a0da69913e01ed630001093ec5037bbf.

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -24,6 +24,4 @@ class Public::SessionsController < Devise::SessionsController
   # def configure_sign_in_params
   #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
   # end
-  
-  
 end
